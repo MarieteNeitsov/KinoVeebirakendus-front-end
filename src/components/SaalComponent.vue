03eeb834c1b0ce73f5b671ae3d625729cc3310b1
marieten <neitsov.mariete@ut.ee>
2024-03-28 08:13:05 +0200

    reccomendig seats implemented

--- src/components/SaalComponent.vue
+++ src/components/SaalComponent.vue
@@ -4,12 +4,16 @@
       <h1>Seansi algus: </h1>
       
       <div class="saalContainer" v-if="istekohad.length">
-        <div class="rida" v-for="reaNumber in istekohad.length / 15" :key="'rida:' + reaNumber">
+        <div class="rida valgetekst" v-for="reaNumber in istekohad.length / 15" :key="'rida:' + reaNumber">
             {{ reaNumber }}
-            <div class="koht" v-for="koht in istekohad.slice((reaNumber- 1) * 15, reaNumber * 15)" :key="koht.id" v-bind:class = "(koht.vaba)?'roheline':'punane'">
+            <div class="koht" v-for="koht in istekohad.slice((reaNumber- 1) * 15, reaNumber * 15)" :key="koht.id" 
+            v-bind:class="{ 'roheline': koht.vaba, 'punane': !koht.vaba, 'kollane': kohtadeNumbrid.includes(koht.kohaNumber) }">
                 {{ koht.kohaNumber }}
             </div>
         </div>
+        <label class="valgetekst" for="kohtadeArv">Sisesta kohtade arv soovituse saamiseks:</label>
+        <input type="number" name="kohteArv" required v-model="kohtadeArv">
+        <button @click="leiaKohad(this.kohtadeArv)" >Soovita!</button>
 
       </div>
     </div>
@@ -21,6 +25,8 @@
     data() {
       return {
         istekohad: [],
+        kohtadeArv: 1,
+        kohtadeNumbrid: [],
         
       };
     },
@@ -39,6 +45,18 @@
                 
                 })
                 .catch((err) => console.log(err.message));
+        },
+        leiaKohad(kohtadeArv){
+            fetch(`http://localhost:8080/saal/${this.$route.params.saalId}/soovita/${kohtadeArv}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    this.kohtadeNumbrid = data
+                    this.fetchSaal()
+                
+                
+                })
+                .catch((err) => console.log(err.message));
+
         }
       
     },
@@ -47,6 +65,13 @@
   </script>
 
   <style scoped>
+  .saalContainer{
+    display: flex;
+    flex-direction: column;
+    padding: 0px;
+    margin: 0px;
+    align-items: center;
+  }
   .rida{
     display: flex;
     flex-direction: row;
@@ -56,6 +81,10 @@
   .koht{
     height: 40px;
     width: 40px;
+    margin-left: 5px;
+    margin-right: 5px;
+    border-radius: 10px;
+
   }
   .roheline{
     background-color: rgb(98, 165, 98);
@@ -63,4 +92,20 @@
   .punane{
     background-color: rgb(185, 120, 120);
   }
+  .kollane{
+    background-color: rgb(222, 209, 92);
+  }
+  input{
+    height: 20px;
+    width: 90px;
+    margin-bottom: 10px;
+  }
+  button{
+    height: 30px;
+    width: 70px;
+    margin-bottom: 10px;
+  }
+ .valgetekst{
+    color:white;
+ }
   </style>
